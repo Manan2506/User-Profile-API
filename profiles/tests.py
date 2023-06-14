c5211befefce50c50bb6c05844a111e9865c8b8c
manangoyal <manangoyal06@gmail.com>
2023-06-14 19:58:57 +0530

    Tests.py updated

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,18 +1,28 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from .models import UserProfile
+from io import BytesIO
+from PIL import Image
 
 class UserProfileAPITests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
+        permission = Permission.objects.get(codename='change_userprofile')
+        self.user.user_permissions.add(permission)
+
         self.client.force_authenticate(user=self.user)
         self.url = reverse('user-profiles')
 
+
     def test_create_user_profile(self):
-        image = SimpleUploadedFile("profile.jpg", b"file_content", "image/png")
+        f = BytesIO()
+        image = Image.new("RGB", (100, 100))
+        image.save(f, 'png')
+        f.seek(0)
+        image = SimpleUploadedFile("profile.jpg", content = f.read() ,)
         data = {
             'name': 'John Doe',
             'email': 'johndoe@example.com',
@@ -29,14 +39,21 @@ class UserProfileAPITests(APITestCase):
         self.assertEqual(UserProfile.objects.get().name, 'John Doe')
 
     def test_update_user_profile(self):
+        f = BytesIO()
+        t_image = Image.new("RGB", (100, 100))
+        t_image.save(f, 'png')
+        f.seek(0)
+        image = SimpleUploadedFile("profile.jpg", content = f.read() ,)
+
         user_profile = UserProfile.objects.create(
             name='Ram',
             email='ram@example.com',
-            bio='Hello, I am Ram.'
+            bio='Hello, I am Ram.',
+            profile_picture = image
         )
 
         update_url = reverse('user-profile-detail', kwargs={'pk': user_profile.pk})
-        image = SimpleUploadedFile("profile.jpg", b"file_content", content_type="image/jpeg")
+        
         data = {
             'name': 'Ravi',
             'email': 'Ravi@example.com',
@@ -44,15 +61,20 @@ class UserProfileAPITests(APITestCase):
             'profile_picture': image
         }
 
-        response = self.client.patch(update_url, data, format='json')
+        response = self.client.patch(update_url, data, format='multipart')
+
+        print(response.content)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(UserProfile.objects.get().name, 'Jane Doe')
-        self.assertEqual(UserProfile.objects.get().email, 'janedoe@example.com')
+        self.assertEqual(UserProfile.objects.get().name, 'Ravi')
+        self.assertEqual(UserProfile.objects.get().email, 'Ravi@example.com')
 
-    image = SimpleUploadedFile("profile.jpg", b"file_content", content_type="image/jpeg")
     def test_invalid_user_profile(self):
-        image = SimpleUploadedFile("profile.jpg", b"file_content", content_type="image/jpeg")
+        f = BytesIO()
+        image = Image.new("RGB", (100, 100))
+        image.save(f, 'png')
+        f.seek(0)
+        image = SimpleUploadedFile("profile.jpg", content = f.read() ,)
         data = {
             'name': 'John Doe',
             'email': 'invalidemail',
@@ -60,7 +82,7 @@ class UserProfileAPITests(APITestCase):
             'profile_picture': image
         }
 
-        response = self.client.post(self.url, data, format='json')
+        response = self.client.post(self.url, data, format='multipart')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(UserProfile.objects.count(), 0)
